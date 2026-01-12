1ed8c9db2aa9a2ec23f38d303d6f6ac9b6d965e5
Wicayonima <alhamdulillahsehatwkwk@gmail.com>
2026-01-12 09:53:02 +0700

    feat: render README as pure JSON

--- generate-readme.js
+++ generate-readme.js
@@ -1,37 +1,11 @@
 import fs from "fs";
 
-const profile = JSON.parse(fs.readFileSync("profile.json", "utf-8"));
+const json = fs.readFileSync("profile.json", "utf-8");
 
 const readme = `
-<h1 align="center">Hi, I'm ${profile.profile.name}</h1>
-
-<p align="center">
-  ${profile.profile.title} • ${profile.profile.focus.join(" • ")}
-</p>
-
-<p align="center">
-  <a href="${profile.profile.website}">
-    ${profile.profile.website}
-  </a>
-</p>
-
----
-
-## About Me
-${profile.about.summary}
-
-**Principles**
-${profile.about.principles.map(p => `- ${p}`).join("\n")}
-
----
-
-## Tech Stack
-${profile.tech_stack.join(", ")}
-
----
-
-## Active Work
-${profile.active_work.map(w => `- ${w}`).join("\n")}
+\`\`\`json
+${json}
+\`\`\`
 `;
 
 fs.writeFileSync("README.md", readme.trim());
