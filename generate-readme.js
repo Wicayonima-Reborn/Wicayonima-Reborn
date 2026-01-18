38cd25291bfaed3e9d24792580c66cc06135f1f9
Wicayonima <alhamdulillahsehatwkwk@gmail.com>
2026-01-18 19:00:14 +0700

    feat: prettify JSON output in README

--- generate-readme.js
+++ generate-readme.js
@@ -1,11 +1,16 @@
 import fs from "fs";
 
-const json = fs.readFileSync("profile.json", "utf-8");
+const raw = fs.readFileSync("profile.json", "utf-8");
+const parsed = JSON.parse(raw);
+
+// prettify JSON
+const pretty = JSON.stringify(parsed, null, 2);
 
 const readme = `
 \`\`\`json
-${json}
+${pretty}
 \`\`\`
 `;
 
 fs.writeFileSync("README.md", readme.trim());
+
